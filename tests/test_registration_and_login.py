4ef8002492c210d95904db56b5959610ea4ed809
Илья <socspec@yandex.ru>
2023-07-20 22:41:58 +0300

    login test

--- tests/test_registration_and_login.py
+++ tests/test_registration_and_login.py
@@ -1,26 +1,30 @@
 import allure
 import pytest
-import time
 from data.registration_data import RegistrationData
 from selenium.webdriver.common.by import By
-
-
 from pages.registration_and_login_page import RegistrationAndLoginPage
 
 
+@pytest.mark.registration_and_login_tests(scope='class', autouse=True)
+@allure.feature('Тесты регистрации и аутентификации нового пользователя.')
 class TestRegistrationAndLoginForm():
 
     main_page_url = 'https://www.demoblaze.com/'
 
-    @pytest.mark.registration_test(scope='function', autouse=True)
+    @allure.story('Тест регистрации нового пользователя.')
     def test_registration_form(self, browser):
         page = RegistrationAndLoginPage(browser, self.main_page_url)
         page.open_page()
         page.registration_new_user()
-        page.is_alert_message_present(RegistrationData.sing_up_successful_message)
+        page.is_alert_message_present(
+            RegistrationData.sing_up_successful_message)
 
+    @allure.story('Тест аутентификации нового пользователя.')
     def test_login_form(self, browser):
         page = RegistrationAndLoginPage(browser, self.main_page_url)
         page.open_page()
         page.login_new_user()
-        page.is_element_present(By.XPATH, f'//button[text()="Welcome {RegistrationAndLoginPage.user_data[0]}"]')
+        page.is_element_present(
+            By.XPATH,
+            f'//button[text()="Welcome \
+            {RegistrationAndLoginPage.user_data[0]}"]')
